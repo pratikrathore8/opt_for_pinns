6a120697c382d172271f7a7b1b793f50c02d5bc7
Pratik Rathore <pratikr@stanford.edu>
2023-10-03 12:16:22 -0700

    SketchySGD w/ strong wolfe appears to work on pinnsformer + 1dreaction

--- pinnsformer_copy/sketchysgd.py
+++ pinnsformer_copy/sketchysgd.py
@@ -17,7 +17,7 @@ class SketchySGD(Optimizer):
         proportional (bool): option to maintain lr to rho ratio, even when lr decays
         chunk_size (int): number of Hessian-vector products to compute in parallel
                           if set to None, binary search will be used to find the maximally allowed value
-        line_search_fn (str): line search function to use (currently only 'backtracking' is supported)
+        line_search_fn (str): line search function to use (currently only 'backtracking', 'strong_wolfe' supported)
         verbose (bool): option to print out eigenvalues of Hessian approximation
     """
     def __init__(self, params, rank = 100, rho = 0.1, lr = 0.01, weight_decay = 0.0,
@@ -44,7 +44,7 @@ class SketchySGD(Optimizer):
         super(SketchySGD, self).__init__(params, defaults)
 
         if self.line_search_fn is not None: 
-            if self.line_search_fn != 'backtracking':
+            if self.line_search_fn not in ['backtracking', 'strong_wolfe']:
                 raise ValueError(f'Line search function {self.line_search_fn} not supported.')
             elif len(self.param_groups) != 1:
                 raise ValueError('Line search only supported for a single parameter group.')
@@ -94,29 +94,36 @@ class SketchySGD(Optimizer):
             direction = -g_new
 
             # Print norms of g and g_new
-            print(f'Norm of g = {torch.norm(g)}')
-            print(f'Norm of g_new = {torch.norm(g_new)}')
+            # print(f'Norm of g = {torch.norm(g)}')
+            # print(f'Norm of g_new = {torch.norm(g_new)}')
             
             # Print cosine similarity between g and g_new
-            print(f'Cosine similarity between g and g_new = {torch.dot(g, g_new) / (torch.norm(g) * torch.norm(g_new))}')
+            # print(f'Cosine similarity between g and g_new = {torch.dot(g, g_new) / (torch.norm(g) * torch.norm(g_new))}')
 
             curr_params = self._clone_param(0)          
-            def obj_func(params_curr, search_dir, step_size):
+            def obj_func(params_curr, step_size, search_dir):
                 # get new parameters
                 self._add_grad(0, step_size, search_dir)
-                loss = float(closure()[0])
+                loss, grad = closure()
+                flat_grad = torch.cat([gradient.view(-1) for gradient in grad if gradient is not None]).detach()
 
                 # reset parameters
                 with torch.no_grad():
                     self._set_param(0, params_curr)
 
-                return loss
+                return float(loss), flat_grad
             
             if self.line_search_fn == 'backtracking':
-                t = self._backtracking(obj_func, loss, curr_params, g, direction, lr)
+                t = self._backtracking(obj_func, loss, curr_params, g, direction, torch.dot(g, direction), lr)
+            elif self.line_search_fn == 'strong_wolfe':
+                t = self._strong_wolfe(obj_func, curr_params, lr, direction, loss, g, torch.dot(g, direction))
 
             # update model parameters
             self._add_grad(0, t, direction)
+
+            # If t is not a tensor, convert it to a tensor
+            if not torch.is_tensor(t):
+                t = torch.tensor(t)
 
             # store step-size in state dict
             self.state[0]['step_size'] = t
@@ -180,8 +187,8 @@ class SketchySGD(Optimizer):
             shift = shift + torch.abs(torch.min(eigs))
             # add shift to eigenvalues
             eigs = eigs + shift
-            print(f'Shift = {shift}')
-            print(f'Eigenvalues = {eigs}')
+            # print(f'Shift = {shift}')
+            # print(f'Eigenvalues = {eigs}')
             # put back the matrix for Cholesky by eigenvector * eigenvalues after shift * eigenvector^T 
             C = torch.linalg.cholesky(torch.mm(eigvectors, torch.mm(torch.diag(eigs), eigvectors.T)))
 
@@ -239,6 +246,10 @@ class SketchySGD(Optimizer):
         return [p.clone(memory_format=torch.contiguous_format) for p in self.param_groups[group_idx]['params']]
     
     def _add_grad(self, group_idx, step_size, update):
+        # TODO: make this less hacky
+        # If step_size is a tensor, convert it to a float
+        if torch.is_tensor(step_size):
+            step_size = step_size.item()
         offset = 0
         for p in self.param_groups[group_idx]['params']:
             numel = p.numel()
@@ -252,22 +263,163 @@ class SketchySGD(Optimizer):
     # Write a backtracking line search that uses the closure as the loss function
     # The line search function should take the following inputs: obj_func, loss_cur, parameters, gradient, search direction, initial step size, and backtracking parameters
     # The line search function should return the appropriate step size
-    def _backtracking(self, obj_func, loss_cur, params, grad, search_dir, init_step_size, alpha=1e-4, beta=0.5):
+    def _backtracking(self, obj_func, loss_cur, params, grad, search_dir, gtd, init_step_size, alpha=1e-4, beta=0.5):
         # initialize step size
         step_size = init_step_size
         # evaluate loss at current parameters
-        loss_new = obj_func(params, search_dir, step_size)
+        loss_new, _ = obj_func(params, step_size, search_dir)
 
         # print(f'loss_new in backtracking ls = {loss_new}')
 
         # while loss at new parameters is greater than loss at current parameters plus sufficient decrease
-        while math.isnan(loss_new) or loss_new > loss_cur + alpha * step_size * torch.dot(grad, search_dir):
+        while math.isnan(loss_new) or loss_new > loss_cur + alpha * step_size * gtd:
             # update step size
             step_size *= beta
             # evaluate loss at new parameters
-            loss_new = obj_func(params, search_dir, step_size)
+            loss_new, _ = obj_func(params, step_size, search_dir)
 
         return step_size
+
+    def _cubic_interpolate(self, f1, f2, g1, g2, x1, x2, bounds=None): 
+        if bounds is not None:
+            xmin_bound, xmax_bound = bounds
+        else:
+            xmin_bound, xmax_bound = (x1, x2) if x1 <= x2 else (x2, x1)
+
+        d1 = g1 + g2 - 3 * (f1 - f2) / (x1 - x2)
+        d2_square = d1**2 - g1 * g2
+            
+        if d2_square >= 0:
+            d2 = d2_square.sqrt()
+            if x1 <= x2:
+                min_pos = x2 - (x2 - x1) * ((g2 + d2 - d1) / (g2 - g1 + 2 * d2))
+            else:
+                min_pos = x1 - (x1 - x2) * ((g1 + d2 - d1) / (g1 - g2 + 2 * d2))
+            return min(max(min_pos, xmin_bound), xmax_bound)
+        else:
+            return (xmin_bound + xmax_bound) / 2.
+        
+    def _strong_wolfe(self, obj_func, x, t, d, f, g, gtd, c1=1e-4, c2=0.9,
+                         tolerance_change=1e-9, max_ls=30): 
+        d_norm = d.abs().max()
+        g = g.clone(memory_format=torch.contiguous_format)
+        f_new, g_new = obj_func(x, t, d)
+        ls_func_evals = 1
+        gtd_new = g_new.dot(d)
+
+        # print(f'gtd = {gtd}')
+        
+        if math.isinf(f_new) or math.isnan(f_new) or torch.isinf(gtd_new) or torch.isnan(gtd_new): 
+            t = t / g.abs().sum()
+            f_new, g_new = obj_func(x, t, d)
+            gtd_new = g_new.dot(d)
+
+        t_prev, f_prev, g_prev, gtd_prev = 0, f, g, gtd
+        done = False
+        ls_iter = 0
+        while ls_iter < max_ls:
+            if f_new > (f + c1 * t * gtd) or (ls_iter > 1 and f_new >= f_prev):
+                bracket = [t_prev, t]
+                bracket_f = [f_prev, f_new]
+                bracket_g = [g_prev, g_new.clone(memory_format=torch.contiguous_format)]
+                bracket_gtd = [gtd_prev, gtd_new]
+                break
+
+            if abs(gtd_new) <= -c2 * gtd:
+                bracket = [t]
+                bracket_f = [f_new]
+                bracket_g = [g_new]
+                done = True
+                break
+
+            if gtd_new >= 0:
+                bracket = [t_prev, t]
+                bracket_f = [f_prev, f_new]
+                bracket_g = [g_prev, g_new.clone(memory_format=torch.contiguous_format)]
+                bracket_gtd = [gtd_prev, gtd_new]
+                break
+
+            min_step = t + 0.01 * (t - t_prev)
+            max_step = t * 10
+            tmp = t
+            t = self._cubic_interpolate(
+                t_prev,
+                f_prev,
+                gtd_prev,
+                t,
+                f_new,
+                gtd_new,
+                bounds=(min_step, max_step))
+
+            t_prev = tmp
+            f_prev = f_new
+            g_prev = g_new.clone(memory_format=torch.contiguous_format)
+            gtd_prev = gtd_new
+            f_new, g_new = obj_func(x, t, d)
+            ls_func_evals += 1
+            gtd_new = g_new.dot(d)
+            ls_iter += 1
+
+        if ls_iter == max_ls:
+            bracket = [0, t]
+            bracket_f = [f, f_new]
+            bracket_g = [g, g_new]
+
+        insuf_progress = False
+        low_pos, high_pos = (0, 1) if bracket_f[0] <= bracket_f[-1] else (1, 0)
+        while not done and ls_iter < max_ls:
+            if abs(bracket[1] - bracket[0]) * d_norm < tolerance_change: break
+
+            t = self._cubic_interpolate(bracket[0], bracket_f[0], bracket_gtd[0],
+                               bracket[1], bracket_f[1], bracket_gtd[1])
+
+            eps = 0.1 * (max(bracket) - min(bracket))
+            if min(max(bracket) - t, t - min(bracket)) < eps:
+                # interpolation close to boundary
+                if insuf_progress or t >= max(bracket) or t <= min(bracket):
+                    # evaluate at 0.1 away from boundary
+                    if abs(t - max(bracket)) < abs(t - min(bracket)):
+                        t = max(bracket) - eps
+                    else:
+                        t = min(bracket) + eps
+                    insuf_progress = False
+                else:
+                    insuf_progress = True
+            else:
+                insuf_progress = False
+
+            f_new, g_new = obj_func(x, t, d)
+            ls_func_evals += 1
+            gtd_new = g_new.dot(d)
+            ls_iter += 1
+
+            if f_new > (f + c1 * t * gtd) or f_new >= bracket_f[low_pos]:
+                bracket[high_pos] = t
+                bracket_f[high_pos] = f_new
+                bracket_g[high_pos] = g_new.clone(memory_format=torch.contiguous_format)
+                bracket_gtd[high_pos] = gtd_new
+                low_pos, high_pos = (0, 1) if bracket_f[0] <= bracket_f[1] else (1, 0)
+            else:
+                if abs(gtd_new) <= -c2 * gtd:
+                    done = True
+                elif gtd_new * (bracket[high_pos] - bracket[low_pos]) >= 0:
+                    # old high becomes new low
+                    bracket[high_pos] = bracket[low_pos]
+                    bracket_f[high_pos] = bracket_f[low_pos]
+                    bracket_g[high_pos] = bracket_g[low_pos]
+                    bracket_gtd[high_pos] = bracket_gtd[low_pos]
+
+                # new point becomes new low
+                bracket[low_pos] = t
+                bracket_f[low_pos] = f_new
+                bracket_g[low_pos] = g_new.clone(memory_format=torch.contiguous_format)
+                bracket_gtd[low_pos] = gtd_new
+
+        t = bracket[low_pos]
+        f_new = bracket_f[low_pos]
+        g_new = bracket_g[low_pos]
+        
+        return t
 
 def hvp(grad_params, params, v):
     Hv = torch.autograd.grad(grad_params, params, grad_outputs = v,
